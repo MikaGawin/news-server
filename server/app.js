723c290edcd038276a1135d0382e7a94a9668f1c
MikaGawin <mikasgawin@gmail.com>
2024-05-28 14:55:39 +0100

    creates /api route, model and controller

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
-const express =require('express');
-const {internalServerError} = require('./errorHandling/errorHandling')
-const {getTopics} = require('./controllers/topics-controllers')
+const express = require("express");
+const { getApi } = require("./controllers/api-controllers");
+const { internalServerError } = require("./errorHandling/errorHandling");
+const { getTopics } = require("./controllers/topics-controllers");
 
 const app = express();
 
-app.get('/api/topics', getTopics);
+app.get("/api", getApi);
+
+app.get("/api/topics", getTopics);
 
 app.use(internalServerError);
 
