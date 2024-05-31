22f42c41ad57d591841a0038f84ddd55fec91cc0
MikaGawin <mikasgawin@gmail.com>
2024-05-31 09:54:54 +0100

    fixes ordering in the connection file

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,20 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
 const config = {};
 
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-
-
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  console.log(config.connectionString);
+  config.max = 2;
+}
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 module.exports = new Pool(config);
